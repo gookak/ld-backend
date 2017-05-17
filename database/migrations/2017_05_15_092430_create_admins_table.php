26c7e35af6d2ea5fcf6d171326206bfe2062d980
yaicece <yaicece22@gmail.com>
2017-05-17 13:28:30 +0700

    edit DB

--- database/migrations/2017_05_15_092430_create_admins_table.php
+++ database/migrations/2017_05_15_092430_create_admins_table.php
@@ -15,11 +15,16 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('role_id')->unsigned();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::table('admins', function($table) {
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
